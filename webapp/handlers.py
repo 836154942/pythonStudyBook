64eac347fcd202f5647b169f11fc18e8342a4ea4
836154942@qq.com <a3071272>
2017-05-22 23:02:19 +0800

    user api test'

--- webapp/handlers.py
+++ webapp/handlers.py
@@ -12,15 +12,27 @@ async def index(request):
         '__template__': 'test.html',
         'users': users
     }
+
+
 @get('/')
 def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
     ]
     return {
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+
+# sql测试
+@get("/api/users")
+async def api_get_users():
+    users = await User.findAll(where='admin =0', orderBy='created_at desc')
+
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)

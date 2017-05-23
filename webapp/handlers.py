6625aef6b25507c6d3939f643b95f186d0e46544
836154942@qq.com <a3071272>
2017-05-23 08:37:36 +0800

    用户注册页面和 数据保存

--- webapp/handlers.py
+++ webapp/handlers.py
@@ -1,5 +1,9 @@
 import re, time, json, logging, hashlib, base64, asyncio
 
+from aiohttp import web
+
+import webapp.coroweb
+from webapp.apis import APIValueError, APIError
 from webapp.coroweb import get, post
 
 from webapp.db.Models import User, Comment, Blog, next_id
@@ -31,8 +35,41 @@ def index(request):
 # sql测试
 @get("/api/users")
 async def api_get_users():
-    users = await User.findAll(where='admin =0', orderBy='created_at desc')
+    users = await User.findAll(where='admin =0', orderBy='created_at desc', limit=(1, 3))
 
     for u in users:
         u.passwd = '******'
     return dict(users=users)
+
+
+@get("/register")
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+# 用户注册
+@post("/api/users")
+def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll("email ='%s'" % email)
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_pwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_pwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from  user.save()
+    print("保存用户成功")
+    # r = web.Response()
+    # r.set_cookie()
